fc8c9d29485afc75c041cb628e8fff65bbe96a3e
Dzifa AA <agbakpedzifaaku3@gmail.com>
2025-12-14 14:45:10 +0000

    Update script.js

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -208,9 +208,11 @@ function postQuoteToServer(quote) {
   return fetch("https://jsonplaceholder.typicode.com/posts", {
     method: "POST",
     body: JSON.stringify(quote),
-    headers: { "Content-type": "application/json; charset=UTF-8" }
+    headers: {
+      "Content-Type": "application/json; charset=UTF-8"
+    }
   })
-    .then(res => res.json());
+  .then(res => res.json());
 }
 
 // ----------------------------------------------------------
